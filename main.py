caee56118c420a4109c3b611c0a14fed68fdd6af
zh Wang <rekind133@outlook.com>
2023-04-21 04:04:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os 
 files = os.listdir()
 os.chdir('..')
+os.system("apt update")
+os.system("apt install fish")
+os.system("chsh -s /usr/bin/fish")
+os.system("apt install build-essential python3-dev python3-pip python-is-python3")
 for file in files:
   if file[0] == '.' and file != '.git':
     try:
