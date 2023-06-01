8c8523e6c07a9dd759bc5a401b121ceaa3e786ea
zh Wang <rekind133@outlook.com>
2023-06-02 00:34:50 +0800

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,15 @@ function fish_prompt -d "Write out the prompt"
 end
 
 set -x PATH . $PATH
+set -x PATH /usr/local/cuda/bin $PATH
+set -x PATH "$HOME/.cargo/bin" $PATH
+set -x PATH "$HOME/.nvim/bin" $PATH
+
+git config --global https.proxy
+git config --global http.sslVerify false
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-# eval /usr/local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /usr/local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
