86fcfb3db8ca612771b761110cee44466fa25302
zh Wang <rekind133@outlook.com>
2023-09-20 11:00:59 +0800

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 function fish_prompt -d "Write out the prompt"
-    set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
     printf '%s%s %s%s%s%s%s: ' $USER \
         (set_color $fish_color_cwd) (prompt_pwd) (set_color FFB6C1) (fish_git_prompt) (set_color normal) 
 end
@@ -14,6 +13,6 @@ git config --global http.sslVerify false
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
