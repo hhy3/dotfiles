a77063af7acad3574f31fe743219f3aa8aae2047
zh Wang <rekind133@outlook.com>
2025-06-29 17:11:42 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.chdir('..')
 os.system("apt update")
 os.system("apt install fish -y")
 os.system("chsh -s /usr/bin/fish")
-os.system("apt install build-essential python3-dev python3-pip python-is-python3 cmake -y")
+os.system("apt install build-essential python3-dev python3-pip python-is-python3 cmake tmux -y")
 for file in files:
   if file[0] == '.' and file != '.git':
     try:
