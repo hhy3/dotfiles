8b6a3531bceec8150eaf9f4fc22dc88c6fd23608
zh Wang <rekind133@outlook.com>
2025-07-09 01:26:24 +0800

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,9 +8,13 @@ set -x PATH "$HOME/.local/bin" $PATH
 set -x PATH /usr/local/cuda/bin $PATH
 set -x PATH "$HOME/.cargo/bin" $PATH
 set -x PATH "$HOME/.nvim/bin" $PATH
+set -x PATH /usr/local/go/bin $PATH
 
 set -x HF_ENDPOINT https://hf-mirror.com
 
+set -x UV_DEFAULT_INDEX https://pypi.tuna.tsinghua.edu.cn/simple
+
 git config --global https.proxy
 git config --global http.sslVerify false
+
 
