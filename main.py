34ad84dc5c80215b82775d26ba359173c188af66
zh Wang <rekind133@outlook.com>
2025-09-13 13:53:08 +0800

    add zsh config

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 files = os.listdir()
 os.chdir('..')
 os.system("apt update")
-os.system("apt install fish -y")
-os.system("chsh -s /usr/bin/fish")
+
+os.system("apt install zsh -y")
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 os.system("apt install build-essential python3-dev python3-pip python-is-python3 cmake tmux -y")
 for file in files:
   if file[0] == '.' and file != '.git':
