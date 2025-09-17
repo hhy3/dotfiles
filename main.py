e7791eb88b78215a68a60b062be6d93ff2942c9e
zh Wang <rekind133@outlook.com>
2025-09-17 18:51:06 +0800

    fix

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-import os 
+import os
+
 files = os.listdir()
-os.chdir('..')
-os.system("apt update")
+os.chdir("..")
+os.system("sudo apt update")
 
-os.system("apt install zsh -y")
-sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+os.system("sudo apt install zsh fzf -y")
+os.system(
+    "sudo apt install build-essential python3-dev python3-pip python-is-python3 cmake tmux -y"
+)
+os.system('sh -c "$(curl -fsSL https://install.ohmyz.sh/)"')
+os.system(
+    "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+)
+os.system(
+    "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+)
 
-os.system("apt install build-essential python3-dev python3-pip python-is-python3 cmake tmux -y")
 for file in files:
-  if file[0] == '.' and file != '.git':
-    try:
-      os.remove(f"{file}")
-    except:
-      pass
-    os.system(f"ln -s .dotfiles/{file} {file}")
-
+    if file[0] == "." and file != ".git":
+        try:
+            os.remove(f"{file}")
+        except:
+            pass
+        os.system(f"ln -s .dotfiles/{file} {file}")
